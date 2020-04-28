bb19651627be2e54b516a2d9b2e2ce8b1373e4e2
Bastian Hodapp <bastianhodapp@gmail.com>
2020-04-28 10:16:42 +0200

    Add namespace

--- L02/main.js
+++ L02/main.js
@@ -1,7 +1,10 @@
 "use strict";
-window.onload = function () {
-    let message = "Hello World!";
-    this.console.log(message);
-    document.getElementById("text").innerHTML = message;
-};
+var L02_HelloWorld;
+(function (L02_HelloWorld) {
+    window.onload = function () {
+        let message = "Hello World!";
+        this.console.log(message);
+        document.getElementById("text").innerHTML = message;
+    };
+})(L02_HelloWorld || (L02_HelloWorld = {}));
 //# sourceMappingURL=main.js.map
--- L02/main.ts
+++ L02/main.ts
@@ -1,5 +1,7 @@
-window.onload = function () {
-    let message: string = "Hello World!";
-    this.console.log(message);
-    document.getElementById("text").innerHTML = message;
-};
+namespace L02_HelloWorld {
+    window.onload = function () {
+        let message: string = "Hello World!";
+        this.console.log(message);
+        document.getElementById("text").innerHTML = message;
+    };
+}
