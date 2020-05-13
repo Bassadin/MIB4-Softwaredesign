c3460ff03a8d8903b691e7024ffcda161771adb0
Bastian Hodapp <bastianhodapp@gmail.com>
2020-05-13 11:01:43 +0200

    PascalCase namespace fix

--- L02/main.ts
+++ L02/main.ts
@@ -1,5 +1,4 @@
-// Namespace in PascalCase
-namespace L02_HelloWorld {
+namespace L02HelloWorld {
     window.onload = function () {
         let message: string = "Hello World!";
         this.console.log(message);
